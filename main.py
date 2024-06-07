097a90785d49190097cdbe28f049371197cebc3d
Trejo Duque Alex Fernando <lanchado10@gmail.com>
2024-06-07 02:13:57 -0500

    Added frontend files and configured Flask to serve them

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World, my name is: Alex Trejo", "Hello World, my name is: Karla Ansatuña"}
-    
-    response = jsonify(list(my_set))
-    response.mimetype = "application/json; charset=utf-8"
-    return response
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
